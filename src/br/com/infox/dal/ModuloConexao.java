7b87e2be3cc2b0933432a2b2406fab6f8467cb92
Keliane <kellisantos023@gmail.com>
2020-11-18 17:23:30 -0200

    primeiro commit

---
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,29 @@
+package br.com.infox.dal;
+
+import java.sql.*;
+
+public class ModuloConexao {
+
+    //método responsavel por estabelecer a conexao com o banco
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+        //a linha abaixo chama o driver 
+        String driver = "com.mysql.jdbc.Driver";
+        //armazenando informacoes referente ao banco
+        String url = "jdbc:mysql://localhost:3306/dbinfox";
+        String user = "root";
+        String password = "";
+        //estabelecendo a conexão com o banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            // a linha abaixo serve de apoio para esclarecer o rerro 
+            //System.out.println(e);
+            return null;
+        }
+
+    }
+
+}
